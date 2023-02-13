cb049149b7e2ca0204cd32db88806ccd69b6395a
CoffeeCat35 <nikitatyshchenkoofficial@gmail.com>
2023-02-13 17:40:57 +0200

    first_module_commit

--- 03_CLI_Telegram_Console_Sender/app.js
+++ 03_CLI_Telegram_Console_Sender/app.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'YOUR TELEGRAM BOT TOKEN';;
+const token = 'YOUR TELEGRAM BOT TOKEN';
 const bot = new TelegramBot(token, { polling: true });
 
 const fs = require('fs');
